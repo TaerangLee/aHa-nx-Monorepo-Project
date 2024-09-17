dd62da3099b0de9dd1586a25579415e3e95b539b
TaerangLee <s22051@gsm.hs.kr>
2024-09-17 20:55:57 +0900

    Add: setting vercel wep domain distribution

--- apps/aHa-ssg/src/pages/_app.tsx
+++ apps/aHa-ssg/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
-  return <div>Hello World 2</div>;
+  return <div>Hello World 2asdasd</div>;
 }
 
 export default CustomApp;
